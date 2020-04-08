a0fb5f7474af377df0efedbf02db627736409254
KyuHyoungKim <61902093+KyuHyoungKim@users.noreply.github.com>
2020-04-08 10:31:53 +0900

    Update N개의 최소공배수.cpp

--- Programmers/Level2/N개의 최소공배수.cpp
+++ Programmers/Level2/N개의 최소공배수.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int Min_Common_Factor(int num1, int num2)
+int Max_Common_Factor(int num1, int num2)
 {
     int ret = 0;
     for (int i = 1; i <= num1; i++)
@@ -19,9 +19,9 @@ int solution(vector<int> arr) {
     int temp = 0;
     if (arr.size() > 1)
     {
-        temp = arr[0] * (arr[1] / Min_Common_Factor(arr[0], arr[1]));
+        temp = arr[0] * (arr[1] / Max_Common_Factor(arr[0], arr[1]));
         for (int i = 1; i < arr.size() - 1; i++)
-            temp *= (arr[i + 1] / Min_Common_Factor(temp, arr[i + 1]));
+            temp *= (arr[i + 1] / Max_Common_Factor(temp, arr[i + 1]));
     }
     else
     {
