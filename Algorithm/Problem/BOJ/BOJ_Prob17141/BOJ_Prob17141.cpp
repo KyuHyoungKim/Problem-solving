b504e6b8dc8df9a39c6507379020adf6fa3f1ca8
규형 김 <kho_ohk@naver.com>
2020-09-27 17:04:05 +0900

    BOJ_Prob17141

---
+++ Algorithm/Problem/BOJ/BOJ_Prob17141/BOJ_Prob17141.cpp
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define INF 1234567891
+#include <cstdio>
+#include <vector>
+#include <queue>
+using namespace std;
+
+const int dy[4] = { -1 ,1, 0, 0 }, dx[4] = { 0, 0, -1, 1 };
+int res = INF;
+int N, M, blank_cnt = 0;
+vector<vector<int>> Map;
+vector<pair<int, int>> Viruses;
+vector<int> selected;
+
+int BFS(vector<vector<int>> map) {
+	int ret = 0;
+	int cnt = 0;
+	queue<pair<int,int>> q;
+	for (int i = 0; i < M; i++) {
+		q.push(Viruses[selected[i]]);
+		map[Viruses[selected[i]].first][Viruses[selected[i]].second] = 1;
+	}
+	while (!q.empty()) {
+		int y = q.front().first;
+		int x = q.front().second;
+		int time = map[y][x];
+		q.pop();
+		cnt++;
+		if (time > ret)
+			ret = time;
+		for (int i = 0; i < 4; i++) {
+			int Y = y + dy[i];
+			int X = x + dx[i];
+			if (Y < 0 || Y >= N || X < 0 || X >= N || map[Y][X])
+				continue;
+			map[Y][X] = time + 1;
+			q.push(make_pair(Y, X));
+		}
+	}
+	ret--;
+	if (cnt != blank_cnt)
+		ret = INF;
+	return ret;
+}
+
+void solution(int cnt, int idx) {
+	if (cnt == M) {
+		int ans = BFS(Map);
+		if (ans < res)
+			res = ans;
+		return;
+	}
+	if (idx >= Viruses.size())
+		return;
+	for (int i = idx; i < Viruses.size(); i++) {
+		selected[cnt] = i;
+		solution(cnt + 1, i + 1);
+	}
+}
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	scanf("%d %d", &N, &M);
+	Map.resize(N);
+	selected.resize(M);
+	for (int i = 0; i < N; i++) {
+		Map[i].resize(N);
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &Map[i][j]);
+			if (Map[i][j] == 2) {
+				Map[i][j] = 0;
+				Viruses.push_back(make_pair(i, j));
+			}
+			if (Map[i][j] == 0)
+				blank_cnt++;
+		}
+	}
+	solution(0, 0);
+	if (res == INF)
+		res = -1;
+	printf("%d", res);
+	return 0;
+}
